026b4bb5ce51d18dbb1f0a9800bfb6d1f5db534c
Joe Inglish <joseph.inglish@gmail.com>
2019-06-06 07:22:18 -0700

    More work... doesn't work

--- src/app.js
+++ src/app.js
@@ -6,6 +6,11 @@ let app = Express();
 let database = new Nedb();
 const APPLICATION_PORT = 3030;
 
+const GAME_NOT_FOUND_MESSAGE = 'Game not found';
+const PLAYER_NOT_IN_GAME_MESSAGE = 'That player is not a participant in this game'
+
+
+
 // Returns all active games
 app.get('/drop_token', (request, response) => {
     // Get and return all games
@@ -13,11 +18,23 @@ app.get('/drop_token', (request, response) => {
 
 // Creates a new game
 app.post('/drop_token', (request, response) => {
-    let game = new Game();
+    let players = request.body.players;
+    let columns = request.body.columns;
+    let rows = request.body.rows;
+
+    if (!columns || !rows || !Array.isArray(players)) {
+        return response.status(400).send('Request body was not properly formatted');
+    }
+
+    let game = new Game(players, columns, rows);
     database.insert(game.toJson(), (error, persistedGame) => {
         if (error) {
             return response.status(500).send(error.message);
         }
+
+        return response.status(200).send({
+            gameId: persistedGame._id
+        });
     });
 });
 
@@ -25,13 +42,13 @@ app.post('/drop_token', (request, response) => {
 app.get('/drop_token/:gameId', (request, response) => {
     // TODO: validate input
 
-    database.fineOne({_id: request.params.gameId}, (error, game) => {
+    database.findOne({_id: request.params.gameId}, (error, game) => {
         if (error) {
             return response.status(500).send(error.message);
         }
 
         if (!game) {
-            return response.status(404).send('Game not found');
+            return response.status(404).send(GAME_NOT_FOUND_MESSAGE);
         }
 
         return response.send(game);
@@ -54,7 +71,30 @@ app.get('/drop_token/:gameId/moves/:moveNumber', (request, response) => {
 });
 
 // Quits the game for a given player
-app.delete('/drop_token/:gameId/:playerId');
+app.delete('/drop_token/:gameId/:playerId', (request, response) => {
+    database.findOne({_id: request.params.gameId}, (error, persistedGame) => {
+        if (error) {
+            return response.status(500).send(error.message);
+        }
+
+        if (!persistedGame) {
+            return response.status(404).send(GAME_NOT_FOUND_MESSAGE);
+        }
+
+        if (persistedGame.player !== request.params.playerId) {
+            return response.status(404).send(PLAYER_NOT_IN_GAME_MESSAGE);
+        }
+
+        if (persistedGame.state === 'DONE') {
+            return response.status(410).send('The game has already been completed');
+        }
+
+        let game = new Game(persistedGame);
+        let newMove = {type: 'QUIT', player: game.player};
+        game.moves.push();
+        game.state = 'DONE';
+    });
+});
 
 const server = app.listen(APPLICATION_PORT, () => {
     console.log('Drop token game running on port ' + server.address().port + '!');
--- src/game.js
+++ src/game.js
@@ -4,24 +4,97 @@ const DEFAULT_ROWS = 4;
 const DEFAULT_COLUMNS = 4;
 const ACTIVE_STATE = 'IN_PROGRESS';
 const DONE_STATE  = 'DONE';
-const PLAYER_1 = 'player1';
-const PLAYER_2 = 'player2';
 
 class Game {
     constructor (gameEntity) {
         if (!arguments.length) {
             this.board = this.getEmptyBoard();
-            this.status = ACTIVE_STATE;
+            this.moves = [];
+            this.state = ACTIVE_STATE;
+            // TODO: fix this
+            this.players = [PLAYER_1, PLAYER_2];
+            this.player = players[0];
             this.winner = null;
+        } else {
+            this._id = gameEntity._id;
+            this.board = gameEntity.board;
+            this.moves = gameEntity.moves;
+            this.state = gameEntity.state;
+            this.players = gameEntity.players;
+            this.winner = gameEntity.winner;
         }
     }
 
     toJson () {
-        // TODO: everything
+        if (this.winner) {
+            return {
+                players: this.players,
+                state: this.state,
+                winner: this.winner
+            };
+        }
+
+        return {
+            players: this.players,
+            state: this.state
+        };
     }
 
-    getEmptyBoard(rows = DEFAULT_ROWS, columns = DEFAULT_COLUMNS) {
+    getEmptyBoard (rows = DEFAULT_ROWS, columns = DEFAULT_COLUMNS) {
         return new Array();
+    }
+
+    playMove (column) {
+        
+    }
+
+    playerHasWon (player) {
+        let COLUMN_START = 0;
+        // Check columns
+        for (let i = 0; i < this.rows; i++) {
+            if ((this.board[i][COLUMN_START] === player)
+                    && (this.board[i][COLUMN_START + 1] === player)
+                    && (this.board[i][COLUMN_START + 2] === player)
+                    && (this.board[i][COLUMN_START + 3] === player)) {
+                this.setWinnerAndDoneState();
+                return true;
+            }
+        }
+
+        let ROW_START = 0;
+        // Check rows
+        for (let i = 0; i < this.columns; i++) {
+            if ((this.board[ROW_START][i] === player)
+                    && (this.board[ROW_START + 1][i] === player)
+                    && (this.board[ROW_START + 2][i] === player)
+                    && (this.board[ROW_START + 3][i] === player))) {
+                this.setWinnerAndDoneState();
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if ((this.board[ROW_START][COLUMN_START] === player)
+                && (this.board[ROW_START + 1][COLUMN_START + 1] === player)
+                && (this.board[ROW_START + 2][COLUMN_START + 2] === player)
+                && (this.board[ROW_START + 3][COLUMN_START + 3] === player)) {
+            this.setWinnerAndDoneState();
+            return true;
+        }
+        /* if ((this.board[ROW_START][COLUMN_START] === player)
+            && (this.board[ROW_START + 1][COLUMN_START + 1] === player)
+            && (this.board[ROW_START + 2][COLUMN_START + 2] === player)
+            && (this.board[ROW_START + 3][COLUMN_START + 3] === player)) {
+            return true;
+        }*/
+    
+        return false;
+    }
+
+    setWinnerAndDoneState () {
+        this.winner = this.player;
+        this.player = null;
+        this.state = DONE_STATE;
     }
 }
 
